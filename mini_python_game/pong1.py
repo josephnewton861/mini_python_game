9bf31cd90a9d80d734955e3575f359dadce4166e
Joe <josephnewton861@yahoo.co.uk>
2020-07-17 14:17:55 +0100

    Added an unlimited scoring system for players

--- mini_python_game/pong1.py
+++ mini_python_game/pong1.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -33,6 +37,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
 ball.dy = -2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+
 
 # Functions
 def paddle_a_up():
@@ -87,11 +100,18 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1 
+        score_a += 1
+        pen.clear()
+        pen.write("Player 1: {}   Player 2: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1 
-    
+        score_b += 1
+        pen.clear()
+        pen.write("Player 1: {}   Player 2: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+
     # Paddle and ball collisions 
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
