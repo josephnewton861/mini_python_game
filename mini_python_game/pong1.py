4827ea531f117a4296d524e57556ccce4da133e0
Joe <josephnewton861@yahoo.co.uk>
2020-07-17 10:51:34 +0100

    Added a key press event for paddles to move up and down

--- mini_python_game/pong1.py
+++ mini_python_game/pong1.py
@@ -16,8 +16,39 @@ paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 # Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+# Keyboard binding paddle_a
+
+wn.listen()
+wn.onkey(paddle_a_up, "w")
+wn.onkey(paddle_a_down, "s")
 
 # Main game loop
 while True:
